43d0bbd76e3c9e585eb89129b35c27703695466d
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-03 17:08:32 +0100

    created load_to_csv and main program

--- imports.py
+++ imports.py
@@ -12,3 +12,15 @@ connection = psycopg2.connect(host='localhost', user='joel',
 def get_cursor(connection: psycopg2.extensions.connection) -> psycopg2.extensions.cursor:
     """Sets up cursor"""
     return connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+
+def load_to_csv(filename: str) -> list[dict]:
+    with open(filename, encoding="utf-8") as f:
+        data = []
+        for line in csv.DictReader(f):
+            data.append(line)
+    return data
+
+
+if __name__ == "__main__":
+    movies = load_to_csv("imdb_movies.csv")
