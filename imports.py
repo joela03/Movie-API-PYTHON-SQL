7d231eb4be8a6fd08223f904ad4dd5056d98f2eb
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-03 16:59:20 +0100

    created get_cursor function

--- imports.py
+++ imports.py
@@ -7,3 +7,8 @@ import psycopg2.extras
 
 connection = psycopg2.connect(host='localhost', user='joel',
                               dbname='movies', port=5432)
+
+
+def get_cursor(connection: psycopg2.extensions.connection) -> psycopg2.extensions.cursor:
+    """Sets up cursor"""
+    return connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
