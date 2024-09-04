095f3f2e5b9a89655ae5d98e53d467d3727ce8ae
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-04 10:58:23 +0100

    created api.py and set up index route

---
+++ api.py
@@ -0,0 +1,17 @@
+"""API that connects to a movie_database"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def endpoint_index():
+    """Sets up index route"""
+    return jsonify({"message": "Welcome to the Movie API"})
+
+
+if __name__ == "__main__":
+    app.config['TESTING'] = True
+    app.config['DEBUG'] = True
+    app.run(debug=True, host="0.0.0.0", port=5000)
