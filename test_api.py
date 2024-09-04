81fdb9a4eb24917a716e074b1873310d40144416
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-04 11:01:30 +0100

    made test_index_route function

--- test_api.py
+++ test_api.py
@@ -9,3 +9,10 @@ def client():
     """Fixture to set up the test client."""
     with app.test_client() as client:
         yield client
+
+
+def test_index_route(client):
+    """Test the index route."""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json == {"message": "Welcome to the Movie API"}
