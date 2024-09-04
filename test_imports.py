26e80e4d06a1cec541a07653b42837d0432c2f63
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-04 10:58:53 +0100

    removed imports.py

--- test_imports.py
+++ test_imports.py
@@ -1,11 +1,1 @@
-import pytest
-from imports import load_to_csv
 
-
-def test_load_to_csv():
-    filename = "test_movies.csv"
-    expected_output = [
-        {"names": "Movie1", "date_x": "2021-01-01"},
-        {"names": "Movie2", "date_x": "2022-02-02"}
-    ]
-    assert load_to_csv(filename) == expected_output
