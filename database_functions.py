24541f4116c37a3a4d070b1ba2ac90ce4e5f5893
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-04 11:28:28 +0100

    made get_movies function

--- database_functions.py
+++ database_functions.py
@@ -8,3 +8,17 @@ def get_connection() -> connection:
     """Sets up connection"""
     return psycopg2.connect(
         "dbname=movie_database user=joel host=localhost")
+
+
+def get_movies() -> list[dict]:
+    """Gets all movies from table"""
+    conn = get_connection()
+    curs = get_cursor(conn)
+
+    query = "SELECT * FROM movie;"
+
+    curs.execute(query)
+    data = curs.fetchall()
+    curs.close()
+
+    return data
