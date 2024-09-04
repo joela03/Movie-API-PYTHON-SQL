74bf5585a6e25fe64c6e3764d3bd0c2f726e14c2
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-04 11:04:40 +0100

    made start start of /movies route

--- api.py
+++ api.py
@@ -11,6 +11,12 @@ def endpoint_index():
     return jsonify({"message": "Welcome to the Movie API"})
 
 
+@app.route("/movies", methods=["GET"])
+def endpoint_get_movies():
+    """Route returns all movies or adds movie to database"""
+    ...
+
+
 if __name__ == "__main__":
     app.config['TESTING'] = True
     app.config['DEBUG'] = True
