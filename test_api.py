d32dfb9f2a95d4834e066672b95d4ee91a3bb000
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-05 11:25:37 +0100

    implemented sort parameter functions and refactored test_movies_sort_order

--- test_api.py
+++ test_api.py
@@ -76,64 +76,62 @@ def test_get_movies_with_search_failure(mock_get_movies, client):
 
 
 @pytest.mark.parametrize("sort_by,sort_order,expected_movies", [
-    ("names", "asc", ["Inception", "Interstellar"]),
-    ("names", "desc", ["Interstellar", "Inception"]),
-    ("date_x", "asc", ["Inception", "Interstellar"]),
-    ("date_x", "desc", ["Interstellar", "Inception"]),
+    ("title", "asc", ["Inception", "Interstellar"]),
+    ("title", "desc", ["Interstellar", "Inception"]),
+    ("release_date", "asc", ["Inception", "Interstellar"]),
+    ("release_date", "desc", ["Interstellar", "Inception"]),
     ("genre", "asc", ["Interstellar", "Inception"]),
     ("genre", "desc", ["Inception", "Interstellar"]),
     ("revenue", "asc", ["Interstellar", "Inception"]),
     ("revenue", "desc", ["Inception", "Interstellar"]),
-    ("budget_x", "asc", ["Inception", "Interstellar"]),
-    ("budget_x", "desc", ["Interstellar", "Inception"]),
+    ("budget", "asc", ["Inception", "Interstellar"]),
+    ("budget", "desc", ["Interstellar", "Inception"]),
     ("score", "asc", ["Interstellar", "Inception"]),
     ("score", "desc", ["Inception", "Interstellar"]),
 ])
 @patch('api.get_movies')
 def test_movies_sort_order(mock_get_movies, client, sort_by, sort_order, expected_movies):
     movie_1 = {
-        "names": "Inception",
-        "date_x": "2010-07-16",
+        "title": "Inception",
+        "release_date": "2010-07-16",
         "score": 8.8,
         "genre": "Science Fiction",
         "overview": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
         "orig_title": "Inception",
         "status": "Released",
         "orig_lang": "English",
-        "budget_x": 160000000,
+        "budget": 160000000,
         "revenue": 829895144,
         "country": "USA"
     }
     movie_2 = {
-        "names": "Interstellar",
-        "date_x": "2014-11-07",
+        "title": "Interstellar",
+        "release_date": "2014-11-07",
         "score": 8.6,
         "genre": "Adventure, Drama, Science Fiction",
         "overview": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
         "orig_title": "Interstellar",
         "status": "Released",
         "orig_lang": "English",
-        "budget_x": 165000000,
+        "budget": 165000000,
         "revenue": 677471339,
         "country": "USA"
     }
 
-    mock_movies = [movie_1, movie_2]
-
     def get_sorted_movies(sort_by, sort_order):
-        if sort_by == "names":
-            return mock_movies if sort_order == "asc" else mock_movies[::-1]
-        if sort_by == "date_x":
+        if sort_by == "title":
+            return [movie_1, movie_2] if sort_order == "asc" else [movie_2, movie_1]
+        if sort_by == "release_date":
             return [movie_1, movie_2] if sort_order == "asc" else [movie_2, movie_1]
         if sort_by == "genre":
             return [movie_2, movie_1] if sort_order == "asc" else [movie_1, movie_2]
         if sort_by == "revenue":
             return [movie_2, movie_1] if sort_order == "asc" else [movie_1, movie_2]
-        if sort_by == "budget_x":
+        if sort_by == "budget":
             return [movie_1, movie_2] if sort_order == "asc" else [movie_2, movie_1]
         if sort_by == "score":
             return [movie_2, movie_1] if sort_order == "asc" else [movie_1, movie_2]
-        return mock_movies
+        return [movie_1, movie_2]
 
     mock_get_movies.side_effect = lambda search, sort_by, sort_order: get_sorted_movies(
         sort_by, sort_order)
@@ -142,6 +140,15 @@ def test_movies_sort_order(mock_get_movies, client, sort_by, sort_order, expecte
     assert response.status_code == 200
     movies = response.get_json()
 
-    returned_movie_titles = [movie["names"] for movie in movies]
+    returned_movie_titles = [movie["title"] for movie in movies]
 
     assert returned_movie_titles == expected_movies
+
+
+@patch('api.get_movies')
+def test_endpoint_invalid_sort_by(mock_get_movies, client):
+    mock_get_movies.return_value = []
+
+    response = client.get('/movies?sort_by=invalid_field')
+    assert response.status_code == 400
+    assert response.get_json() == {"error": "Invalid sort_by parameter"}
