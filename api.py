552d2c74256ba72b9541d874e3771423b8fb9c7b
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-05 11:30:23 +0100

    adding post route to movies

--- api.py
+++ api.py
@@ -12,25 +12,28 @@ def endpoint_index():
     return jsonify({"message": "Welcome to the Movie API"})
 
 
-@app.route("/movies", methods=["GET"])
+@app.route("/movies", methods=["GET", "POST"])
 def endpoint_get_movies():
     """Route returns all movies or adds movie to database"""
     # Adding parameter's to movies route
-    search = request.args.get("search")
-    sort_by = request.args.get("sort_by")
-    sort_order = request.args.get("sort_order")
+    if request.method == "GET":
+        search = request.args.get("search")
+        sort_by = request.args.get("sort_by")
+        sort_order = request.args.get("sort_order")
 
-    if not validate_sort_by(sort_by):
-        return jsonify({"error": "Invalid sort_by parameter"}), 400
+        if not validate_sort_by(sort_by):
+            return jsonify({"error": "Invalid sort_by parameter"}), 400
 
-    if not validate_sort_order(sort_order):
-        return jsonify({"error": "Invalid sort_order parameter"}), 400
+        if not validate_sort_order(sort_order):
+            return jsonify({"error": "Invalid sort_order parameter"}), 400
 
-    movies = get_movies(search, sort_by, sort_order)
-    if movies == []:
-        return {"error": True, "message": "Movies not found"}, 404
+        movies = get_movies(search, sort_by, sort_order)
+        if movies == []:
+            return {"error": True, "message": "Movies not found"}, 404
 
-    return jsonify(movies), 200
+        return jsonify(movies), 200
+    else:
+        data = request.json()
 
 
 if __name__ == "__main__":
